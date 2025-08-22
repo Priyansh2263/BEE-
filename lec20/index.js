90d855cce333f3a3b93cb36bcb9154c08b15926c
Priyansh Sharma <priyansh2263.be23@chitkara.edu.in>
2025-08-22 10:38:30 +0530

    lec20 upload

---
+++ lec20/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const { m1, m2 } = require("./middleware/firstmiddleware");
+const {m3} = require("./middleware/pathlevel");
+const app = express();
+app.use(express.static(__dirname+"/public"))
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(m1)
+
+const userRouter = require("./routes/userRoutes")
+
+app.use("/api/users", userRouter)
+
+app.get("/health",m3,(req,res,next)=>{
+    console.log("running controller function")
+    next()
+    res.json({
+        status:"ok",
+        message:"server running ok"
+    })
+    console.log("after response")
+})
+app.get("/home",(req,res,next)=>{
+    console.log("running home endpoint...");
+    res.json({
+        success:true,
+        message:"welcome to home page"
+    })
+
+})
+app.use(m2)
+
+
+
+app.listen(3001,()=>{
+    console.log("server started at http://localhost:3001")
+})
